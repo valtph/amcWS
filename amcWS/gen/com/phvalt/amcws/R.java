c4ad2e1c48bbde8086c71dd1305f6bb916ff71b7
valtph <phvalt@live.fr>
2015-02-03 22:33:08 +0100

    Sort files
    folder processing, damned git on desktop

--- amcWS/gen/com/phvalt/amcws/R.java
+++ amcWS/gen/com/phvalt/amcws/R.java
@@ -26,21 +26,28 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f080011;
+        public static final int button1=0x7f080010;
         public static final int editText1=0x7f080003;
         public static final int editText2=0x7f080005;
         public static final int editText3=0x7f080007;
         public static final int editText4=0x7f080009;
+        public static final int image=0x7f08000d;
+        public static final int list=0x7f08000f;
         public static final int paramLayout=0x7f080001;
         public static final int srchBtn=0x7f08000a;
+        public static final int text=0x7f08000e;
         public static final int textView2=0x7f080004;
         public static final int textView3=0x7f080006;
         public static final int textView4=0x7f080008;
         public static final int textview1=0x7f080002;
-        public static final int tryReponse=0x7f08000b;
+        public static final int tryReponse=0x7f08000c;
+        public static final int viewBtn=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int item=0x7f030001;
+        public static final int show_list=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
